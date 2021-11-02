0dd0c7b24311d04d9acadbc3e913989cf97d6abc
Thibaud Av <thibaud.avenier@gmail.com>
2021-11-02 16:28:38 +0100

    chore: add test steps in circleci config

--- ui-particles-angular/projects/ui-particles-angular/src/lib/ui-particles-angular.service.ts
+++ ui-particles-angular/projects/ui-particles-angular/src/lib/ui-particles-angular.service.ts
@@ -3,9 +3,4 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root',
 })
-export class UiParticlesAngularService {
-  constructor(sss: any) {
-    let rr;
-  }
-  ss: any;
-}
+export class UiParticlesAngularService {}
