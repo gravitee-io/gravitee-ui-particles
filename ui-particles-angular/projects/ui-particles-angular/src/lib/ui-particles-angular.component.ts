7368ee690ce5aec762e459e52f94495e56f705c5
Thibaud Av <thibaud.avenier@gmail.com>
2021-11-04 09:47:07 +0100

    refactor: remove default default angular stuf

--- ui-particles-angular/projects/ui-particles-angular/src/lib/ui-particles-angular.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'lib-ui-particles-angular',
-  template: ` <p>ui-particles-angular works!</p> `,
-  styles: [],
-})
-export class UiParticlesAngularComponent {}
