abc015bddc68500f83d828d0387b2a324fbc460b
Thibaud Av <thibaud.avenier@gmail.com>
2021-11-04 09:41:39 +0100

    fix: angular testing with jest

--- ui-particles-angular/projects/ui-particles-angular/src/setup-jest.ts
+++ ui-particles-angular/projects/ui-particles-angular/src/setup-jest.ts
@@ -1,1 +1,41 @@
-import 'jest-preset-angular/setup-jest';
+Object.defineProperty(window, 'CSS', { value: null });
+Object.defineProperty(document, 'doctype', {
+  value: '<!DOCTYPE html>',
+});
+Object.defineProperty(window, 'getComputedStyle', {
+  value: () => {
+    return {
+      getPropertyValue: (prop: unknown) => {
+        return '';
+      },
+    };
+  },
+});
+/**
+ * ISSUE: https://github.com/angular/material2/issues/7101
+ * Workaround for JSDOM missing transform property
+ */
+Object.defineProperty(document.body.style, 'transform', {
+  value: () => {
+    return {
+      enumerable: true,
+      configurable: true,
+    };
+  },
+});
+
+/**
+ * ISSUE: https://github.com/thymikee/jest-preset-angular/issues/79
+ *Q: "Could not find Angular Material core theme.." or "Could not find HammerJS" when testing components which used Material Design lib.
+ */
+const WARN_SUPPRESSING_PATTERNS = [/Could not find Angular Material core theme/, /Could not find HammerJS/];
+
+const warn = console.warn;
+
+Object.defineProperty(console, 'warn', {
+  value: (...params: string[]) => {
+    if (!WARN_SUPPRESSING_PATTERNS.some(pattern => pattern.test(params[0]))) {
+      warn(...params);
+    }
+  },
+});
