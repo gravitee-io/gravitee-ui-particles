1aeb58811d98f3ac1a0d5bb141f1941a658ab297
Thibaud Av <thibaud.avenier@gmail.com>
2021-11-04 09:47:07 +0100

    refactor: remove default default angular stuf

--- ui-particles-angular/projects/ui-particles-angular/src/lib/ui-particles-angular.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { UiParticlesAngularComponent } from './ui-particles-angular.component';
-
-@NgModule({
-  declarations: [UiParticlesAngularComponent],
-  imports: [],
-  exports: [UiParticlesAngularComponent],
-})
-export class UiParticlesAngularModule {}
--- ui-particles-angular/projects/ui-particles-angular/src/public-api.ts
+++ ui-particles-angular/projects/ui-particles-angular/src/public-api.ts
@@ -2,6 +2,4 @@
  * Public API Surface of ui-particles-angular
  */
 
-export * from './lib/ui-particles-angular.service';
-export * from './lib/ui-particles-angular.component';
-export * from './lib/ui-particles-angular.module';
+export * from './lib/public-api';
