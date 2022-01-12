1a922c62db48f6f05f6324e26829285286a7b47c
Thibaud Av <thibaud.avenier@gmail.com>
2022-01-12 16:26:29 +0100

    chore: add main Readme and init Get Started into storybook

--- ui-particles-angular/.storybook/main.ts
+++ ui-particles-angular/.storybook/main.ts
@@ -1,7 +1,13 @@
 module.exports = {
   framework: '@storybook/angular',
   stories: ['../projects/**/*.stories.@(ts|mdx)'],
-  addons: ['@storybook/addon-essentials'],
+  addons: [
+    '@storybook/addon-essentials',
+    {
+      name: '@storybook/addon-docs',
+      options: { transcludeMarkdown: true },
+    },
+  ],
   features: {
     previewCsfV3: true,
     storyStoreV7: true,
