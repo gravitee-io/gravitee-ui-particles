27e97bec1da650653ba4a9216c6d99decdce6c65
Thibaud Av <thibaud.avenier@gmail.com>
2022-02-14 14:09:02 +0100

    feat(mat-tabs): override material to remove tabs label min-width

---
+++ ui-particles-angular/projects/ui-particles-angular/src/scss/mat-override/mat-tabs.stories.ts
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { Meta, moduleMetadata } from '@storybook/angular';
+import { Story } from '@storybook/angular/types-7-0';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatTabsModule } from '@angular/material/tabs';
+
+export default {
+  title: 'Material Override',
+  decorators: [
+    moduleMetadata({
+      imports: [BrowserAnimationsModule, MatTabsModule],
+    }),
+  ],
+} as Meta;
+
+export const MatTabs: Story = {
+  render: () => ({
+    template: `
+    <p>
+      Unset mat tab label min-width
+    </p>
+    
+
+    <mat-tab-group mat-align-tabs="start">
+      <mat-tab label="First">Content 1</mat-tab>
+      <mat-tab label="Second">Content 2</mat-tab>
+      <mat-tab label="Third">Content 3</mat-tab>
+    </mat-tab-group>
+    
+    <mat-tab-group mat-align-tabs="center">
+      <mat-tab label="First">Content 1</mat-tab>
+      <mat-tab label="Second">Content 2</mat-tab>
+      <mat-tab label="Third">Content 3</mat-tab>
+    </mat-tab-group>
+    
+    <mat-tab-group mat-align-tabs="end">
+      <mat-tab label="First">Content 1</mat-tab>
+      <mat-tab label="Second">Content 2</mat-tab>
+      <mat-tab label="Third">Content 3</mat-tab>
+    </mat-tab-group>  
+    `,
+  }),
+};
