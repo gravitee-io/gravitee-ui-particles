16698d5b8ea0420f03ea62ed446a9c84d6df6cda
ThibaudAv <thibaud.avenier@gmail.com>
2023-01-12 16:06:38 +0100

    chore: bump fontsource dependencies

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,5 +2,7 @@ module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'body-max-line-length': [2, 'always', 1080],
+    'subject-empty': [0],
+    'type-empty': [0],
   },
 };
