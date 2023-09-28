2594d0c1470620d2f6223da4af20e20a727bef21
ThibaudAv <thibaud.avenier@gmail.com>
2023-09-28 23:16:05 +0200

    feat: init empty form cron component

---
+++ ui-particles-angular/projects/ui-particles-angular/src/lib/gio-form-cron/gio-form-cron.harness.ts
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { BaseHarnessFilters, ComponentHarness, HarnessPredicate } from '@angular/cdk/testing';
+
+export type GioFormCronHarnessFilters = BaseHarnessFilters;
+
+export class GioFormCronHarness extends ComponentHarness {
+  public static hostSelector = 'gio-form-cron';
+
+  /**
+   * Gets a `HarnessPredicate` that can be used to search for this harness.
+   *
+   * @param options Options for filtering which input instances are considered a match.
+   * @return a `HarnessPredicate` configured with the given options.
+   */
+  public static with(options: GioFormCronHarnessFilters = {}): HarnessPredicate<GioFormCronHarness> {
+    return new HarnessPredicate(GioFormCronHarness, options);
+  }
+}
---
+++ ui-particles-angular/projects/ui-particles-angular/src/lib/gio-form-cron/gio-form-cron.module.spec.ts
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { HarnessLoader } from '@angular/cdk/testing';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
+import { Component } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
+import { GioFormCronHarness } from './gio-form-cron.harness';
+import { GioFormCronModule } from './gio-form-cron.module';
+
+describe('GioFormCronModule', () => {
+  @Component({
+    template: ` <gio-form-cron></gio-form-cron> `,
+  })
+  class TestComponent {
+    public control = new FormControl(null, Validators.required);
+  }
+
+  let component: TestComponent;
+  let fixture: ComponentFixture<TestComponent>;
+  let loader: HarnessLoader;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [TestComponent],
+      imports: [NoopAnimationsModule, GioFormCronModule, ReactiveFormsModule],
+    });
+    fixture = TestBed.createComponent(TestComponent);
+    component = fixture.componentInstance;
+    loader = TestbedHarnessEnvironment.loader(fixture);
+  });
+
+  it('should init component', async () => {
+    const formCronHarness = await loader.getHarness(GioFormCronHarness);
+
+    expect(formCronHarness).toBeTruthy();
+    expect(component).toBeTruthy();
+  });
+});
