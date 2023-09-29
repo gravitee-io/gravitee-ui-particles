675150e2c75eedd67d9aea05fba3a6287ad47e2e
ThibaudAv <thibaud.avenier@gmail.com>
2023-09-29 16:28:29 +0200

    feat: cron-form - impl default value for each mode

--- ui-particles-angular/projects/ui-particles-angular/src/lib/gio-form-cron/gio-form-cron.component.ts
+++ ui-particles-angular/projects/ui-particles-angular/src/lib/gio-form-cron/gio-form-cron.component.ts
@@ -19,6 +19,10 @@ import { FocusMonitor } from '@angular/cdk/a11y';
 import { Component, ElementRef, OnDestroy, OnInit, Optional, Self } from '@angular/core';
 import { ControlValueAccessor, FormControl, FormGroup, NgControl } from '@angular/forms';
 import { range } from 'lodash';
+import { Subject } from 'rxjs';
+import { takeUntil, tap } from 'rxjs/operators';
+
+import { getDefaultCronDisplay } from './gio-form-cron.adapter';
 
 @Component({
   selector: 'gio-form-cron',
@@ -30,18 +34,18 @@ export class GioFormCronComponent implements ControlValueAccessor, OnInit, OnDes
 
   public _onTouched: () => void = () => ({});
 
-  public seconds = [...range(1, 60)];
-  public minutes = [...range(1, 60)];
-  public hours = [...range(1, 24)];
-  public daysOfMouth = [...range(1, 32)];
+  public seconds = [...range(0, 59)];
+  public minutes = [...range(0, 59)];
+  public hours = [...range(0, 23)];
+  public daysOfMonth = [...range(1, 32)];
   public daysOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+  public internalFormGroup?: FormGroup;
 
   private touched = false;
   private focused = false;
-
   private value?: string;
 
-  public internalFormGroup?: FormGroup;
+  private unsubscribe$ = new Subject<void>();
 
   constructor(
     @Optional() @Self() public readonly ngControl: NgControl,
@@ -65,17 +69,48 @@ export class GioFormCronComponent implements ControlValueAccessor, OnInit, OnDes
   public ngOnInit(): void {
     this.internalFormGroup = new FormGroup({
       mode: new FormControl('second'),
+      secondInterval: new FormControl(),
+      minuteInterval: new FormControl(),
+      hourInterval: new FormControl(),
+      dayInterval: new FormControl(),
+      dayOfWeek: new FormControl(),
+      dayOfMonth: new FormControl(),
+
       hours: new FormControl(),
       minutes: new FormControl(),
-      seconds: new FormControl(),
-      dayOfMonth: new FormControl(),
-      dayOfWeek: new FormControl(),
       cronExpression: new FormControl('* * * * * *'),
     });
+
+    this.internalFormGroup
+      .get('mode')
+      ?.valueChanges.pipe(
+        tap(mode => {
+          const d = getDefaultCronDisplay(mode);
+
+          this.internalFormGroup?.patchValue(
+            {
+              secondInterval: d.secondInterval,
+              minuteInterval: d.minuteInterval,
+              hourInterval: d.hourInterval,
+              dayInterval: d.dayInterval,
+              dayOfWeek: d.dayOfWeek,
+              dayOfMonth: d.dayOfMonth,
+
+              hours: d.hours,
+              minutes: d.minutes,
+            },
+            { emitEvent: false },
+          );
+        }),
+        takeUntil(this.unsubscribe$),
+      )
+      .subscribe();
   }
 
   public ngOnDestroy(): void {
     this.fm.stopMonitoring(this.elRef.nativeElement);
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
   }
 
   // From ControlValueAccessor interface
