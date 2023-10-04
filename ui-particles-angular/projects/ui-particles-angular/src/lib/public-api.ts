fe8b02411dedf2cf1b9e9183f7ac6426aa3cc651
ThibaudAv <thibaud.avenier@gmail.com>
2023-10-04 18:33:13 +0200

    fix: cron-form - disabled state without initial value

--- ui-particles-angular/projects/ui-particles-angular/src/lib/public-api.ts
+++ ui-particles-angular/projects/ui-particles-angular/src/lib/public-api.ts
@@ -95,5 +95,7 @@ export * from './gio-license/gio-license-dialog/gio-license-dialog.component';
 export * from './gio-license/gio-license.testing.module';
 
 export * from './gio-form-cron/gio-form-cron.component';
+export * from './gio-form-cron/gio-form-cron-label.component';
+export * from './gio-form-cron/gio-form-cron-hint.component';
 export * from './gio-form-cron/gio-form-cron.module';
 export * from './gio-form-cron/gio-form-cron.harness';
