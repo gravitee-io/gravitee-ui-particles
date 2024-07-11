1b74fdabf775b8c01891e539aa358eae2abb1d6f
ThibaudAv <thibaud.avenier@gmail.com>
2024-07-11 17:28:34 +0200

    feat: impl simple REQUEST phase for GioEnvironmentFlowStudioComponent

--- projects/ui-policy-studio-angular/src/lib/environment-flow-studio/gio-environment-flow-studio.component.spec.ts
+++ projects/ui-policy-studio-angular/src/lib/environment-flow-studio/gio-environment-flow-studio.component.spec.ts
@@ -14,24 +14,105 @@
  * limitations under the License.
  */
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
+import { of } from 'rxjs';
+import { SimpleChange } from '@angular/core';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { MatIconTestingModule } from '@angular/material/icon/testing';
+import { MatInputHarness } from '@angular/material/input/testing';
 
-import { GioEnvironmentFlowStudioComponent } from './gio-environment-flow-studio.component';
+import { fakePolicySchema } from '../models/policy/PolicySchema.fixture';
+import { fakeAllPolicies, fakeTestPolicy } from '../models/policy/Policy.fixture';
+
+import { GioEnvironmentFlowStudioHarness } from './gio-environment-flow-studio.harness';
+import { EnvironmentFlowOutput, GioEnvironmentFlowStudioComponent } from './gio-environment-flow-studio.component';
 
 describe('GioEnvironmentFlowStudioComponent', () => {
   let component: GioEnvironmentFlowStudioComponent;
   let fixture: ComponentFixture<GioEnvironmentFlowStudioComponent>;
+  let environmentFlowStudioHarness: GioEnvironmentFlowStudioHarness;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [GioEnvironmentFlowStudioComponent],
+      imports: [GioEnvironmentFlowStudioComponent, NoopAnimationsModule, HttpClientTestingModule, MatIconTestingModule],
     }).compileComponents();
 
     fixture = TestBed.createComponent(GioEnvironmentFlowStudioComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    environmentFlowStudioHarness = await TestbedHarnessEnvironment.harnessForFixture(fixture, GioEnvironmentFlowStudioHarness);
+
+    component.policies = fakeAllPolicies();
+    component.policySchemaFetcher = policy => of(fakePolicySchema(policy.id));
+    component.policyDocumentationFetcher = policy => of(`${policy.id} documentation`);
+    component.ngOnChanges({
+      policies: new SimpleChange(null, null, true),
+      policySchemaFetcher: new SimpleChange(null, null, true),
+      policyDocumentationFetcher: new SimpleChange(null, null, true),
+    });
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should display empty flow phase', async () => {
+    const flowPhase = await environmentFlowStudioHarness.getFlowPhase('REQUEST');
+
+    expect(flowPhase).toBeDefined();
+    const steps = await flowPhase!.getSteps();
+
+    expect(steps).toEqual([
+      {
+        name: 'Incoming request',
+        type: 'connector',
+      },
+      {
+        name: 'Outgoing request',
+        type: 'connector',
+      },
+    ]);
+  });
+
+  it('should add flow to phase', async () => {
+    const flowPhase = await environmentFlowStudioHarness.getFlowPhase('REQUEST');
+
+    let expectedEnvironmentFlowOutput: EnvironmentFlowOutput | undefined = undefined;
+
+    component.environmentFlowChange.subscribe(environmentFlow => (expectedEnvironmentFlowOutput = environmentFlow));
+
+    await environmentFlowStudioHarness?.addStep(0, {
+      policyName: fakeTestPolicy().name,
+      description: 'What does the 🦊 say?',
+      async waitForPolicyFormCompletionCb(locator) {
+        // "Policy to test UI" have required configuration fields. We need to fill them to be able to add the step.
+        const testPolicyConfigurationInput = await locator.locatorFor(MatInputHarness.with({ selector: '[id*="test"]' }))();
+        await testPolicyConfigurationInput.setValue('🦊');
+      },
+    });
+
+    expect(await flowPhase?.getSteps()).toEqual([
+      {
+        name: 'Incoming request',
+        type: 'connector',
+      },
+      {
+        type: 'step',
+        name: fakeTestPolicy().name,
+        hasCondition: false,
+        description: 'What does the 🦊 say?',
+      },
+      {
+        name: 'Outgoing request',
+        type: 'connector',
+      },
+    ]);
+    expect(expectedEnvironmentFlowOutput).toEqual([
+      {
+        configuration: {
+          test: '🦊',
+        },
+        description: 'What does the 🦊 say?',
+        enabled: true,
+        name: 'Policy to test UI',
+        policy: 'test-policy',
+      },
+    ]);
   });
 });
---
+++ projects/ui-policy-studio-angular/src/lib/environment-flow-studio/gio-environment-flow-studio.harness.ts
@@ -0,0 +1,73 @@
+/*
+ * Copyright (C) 2024 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { ComponentHarness } from '@angular/cdk/testing';
+import { MatTabGroupHarness } from '@angular/material/tabs/testing';
+import { DivHarness } from '@gravitee/ui-particles-angular/testing';
+import { MatButtonHarness } from '@angular/material/button/testing';
+
+import { GioPolicyStudioDetailsPhaseHarness, PhaseType } from '../components/flow-details-phase/gio-ps-flow-details-phase.harness';
+import { StepForm } from '../components/step-form/gio-ps-step-form.harness';
+import { GioPolicyStudioPoliciesCatalogDialogHarness } from '../components/policies-catalog-dialog/gio-ps-policies-catalog-dialog.harness';
+
+export class GioEnvironmentFlowStudioHarness extends ComponentHarness {
+  public static hostSelector = 'gio-environment-flow-studio';
+
+  private phaseHarness = (phaseType: PhaseType) => this.locatorFor(GioPolicyStudioDetailsPhaseHarness.with({ type: phaseType }))();
+
+  /**
+   * Get flow phase harness
+   * @param phaseType Phase type to get
+   */
+  public async getFlowPhase(phaseType: PhaseType): Promise<GioPolicyStudioDetailsPhaseHarness | undefined> {
+    if (phaseType === 'PUBLISH' || phaseType === 'SUBSCRIBE') {
+      const matTabsHarness = await this.locatorFor(MatTabGroupHarness.with({ selector: '.content__tabs' }))();
+      await matTabsHarness.selectTab({ label: 'Event messages' });
+    }
+
+    const steps = await this.phaseHarness(phaseType);
+
+    return steps ? steps : undefined;
+  }
+
+  public async clickAddStep(index: number): Promise<void> {
+    const addStepBtn = await this.locatorForAll(DivHarness.with({ selector: '.content__step' }))().then(divs => divs[index]);
+
+    await addStepBtn
+      .childLocatorFor(MatButtonHarness.with({ selector: '.content__step__addBtn' }))()
+      .then(btn => btn.click());
+  }
+
+  /**
+   * Add a step to a phase
+   * @param index Index where to add the step. (Add button index)
+   * @param stepConfig Step to add
+   */
+  public async addStep(
+    index: number,
+    stepConfig: {
+      policyName: string;
+    } & StepForm,
+  ): Promise<void> {
+    await this.clickAddStep(index);
+
+    const catalogDialog = await this.documentRootLocatorFactory().locatorFor(GioPolicyStudioPoliciesCatalogDialogHarness)();
+    await catalogDialog.selectPolicy(stepConfig.policyName);
+
+    await (await catalogDialog.getStepForm()).setStepForm(stepConfig);
+
+    await catalogDialog.clickAddPolicyButton();
+  }
+}
