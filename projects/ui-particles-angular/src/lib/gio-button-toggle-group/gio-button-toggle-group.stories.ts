5b36bb572ff332422caf63ff5c39c0cf30a248d8
Guillaume Cusnieux <guillaume.cusnieux@graviteesource.com>
2025-04-09 14:52:33 +0200

    fix: fix display of gio-button-toggle-group component with only icons

--- projects/ui-particles-angular/src/lib/gio-button-toggle-group/gio-button-toggle-group.stories.ts
+++ projects/ui-particles-angular/src/lib/gio-button-toggle-group/gio-button-toggle-group.stories.ts
@@ -15,25 +15,35 @@
  */
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { Meta, moduleMetadata, StoryObj } from '@storybook/angular';
+import { GioIconsModule } from '@gravitee/ui-particles-angular';
 
 export default {
   title: 'Components / Button Toggle Group',
   decorators: [
     moduleMetadata({
-      imports: [MatButtonToggleModule],
+      imports: [MatButtonToggleModule, GioIconsModule],
     }),
   ],
   render: () => ({}),
 } as Meta;
 
-export const Default: StoryObj = {
+export const All: StoryObj = {
   render: () => ({
     template: `
+    <h4>Text Only</h4>
     <p style="background-color: #fff; padding:16px;">
       <mat-button-toggle-group class="gio-button-toggle-group" aria-label="Font Style">
         <mat-button-toggle value="bold">Bold</mat-button-toggle>
         <mat-button-toggle value="italic">Italic</mat-button-toggle>
         <mat-button-toggle value="underline">Underline</mat-button-toggle>
+      </mat-button-toggle-group>
+    </p>
+    
+    <h4>Icons Only</h4>
+    <p style="background-color: #fff; padding:16px;">
+      <mat-button-toggle-group class="gio-button-toggle-group-icons" aria-label="Font Style" hideSingleSelectionIndicator="true">
+        <mat-button-toggle value="LIST"><mat-icon svgIcon="gio:list"></mat-icon></mat-button-toggle>
+        <mat-button-toggle value="TILE"><mat-icon svgIcon="gio:4x4-cell"></mat-icon></mat-button-toggle>
       </mat-button-toggle-group>
     </p>
     `,
