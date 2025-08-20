d73e0fea7ac049f3400ee87097691d36e397e02f
Michel Barret <michel.barret@graviteesource.com>
2025-08-20 12:00:33 +0200

    feat: create the prompt EL component

---
+++ projects/ui-particles-angular/src/lib/gio-el/gio-el-prompt/gio-el-prompt.stories.ts
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { Meta, moduleMetadata, StoryObj } from '@storybook/angular';
+
+import { GioPopoverModule } from '../../gio-popover/gio-popover.module';
+import { GioElModule } from '../gio-el.module';
+import { GioClipboardModule } from '../../gio-clipboard/gio-clipboard.module';
+
+import { GioElPromptComponent } from './gio-el-prompt.component';
+
+export default {
+  title: 'Components / EL / Prompt',
+  component: GioElPromptComponent,
+  decorators: [
+    moduleMetadata({
+      imports: [GioPopoverModule, GioElModule, GioClipboardModule],
+      declarations: [],
+    }),
+  ],
+  render: () => ({}),
+} as Meta;
+
+export const Prompt: StoryObj = {
+  render: () => ({
+    template: `
+<gio-el-prompt/>
+      `,
+  }),
+};
+
+export const PromptInPopover: StoryObj = {
+  render: () => ({
+    template: `
+    <button gioPopoverTrigger [gioPopoverTriggerFor]="aiPopover">
+      Help me
+    </button>
+
+<gio-popover #aiPopover [closeOnBackdropClick]="true">
+  <gio-el-prompt/>
+</gio-popover>
+      `,
+  }),
+};
